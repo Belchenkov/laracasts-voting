b49e2dc8a0d2c56651316a22e7ae3c2d68ada092
Алексей Бельченков <u608110@gmail.com>
2022-06-05 15:03:43 +0400

    Check if idea voted for

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreIdeaRequest;
 use App\Http\Requests\UpdateIdeaRequest;
 use App\Models\Idea;
+use App\Models\Vote;
 
 class IdeaController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function index()
     {
         $ideas = Idea::with('category', 'user', 'status')
+            ->addSelect([
+                'voted_by_user' => Vote::select('id')
+                    ->where('user_id', auth()->id())
+                    ->whereColumn('idea_id', 'ideas.id')
+            ])
             ->withCount('votes')
             ->orderBy('id', 'desc')
             ->simplePaginate(Idea::PAGINATION_COUNT);
