28334215145ea39b80ca9968ef2b4d986a41bba6
Алексей Бельченков <u608110@gmail.com>
2022-07-03 10:35:38 +0400

    Make livewire component to fetch ideas

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreIdeaRequest;
-use App\Http\Requests\UpdateIdeaRequest;
 use App\Models\Idea;
-use App\Models\Vote;
 
 class IdeaController extends Controller
 {
@@ -16,18 +13,7 @@ class IdeaController extends Controller
      */
     public function index()
     {
-        $ideas = Idea::with('category', 'user', 'status')
-            ->addSelect([
-                'voted_by_user' => Vote::select('id')
-                    ->where('user_id', auth()->id())
-                    ->whereColumn('idea_id', 'ideas.id')
-            ])
-            ->withCount('votes')
-            ->orderBy('id', 'desc')
-            ->simplePaginate(Idea::PAGINATION_COUNT);
-
-        return view('idea.index')
-            ->with(compact('ideas'));
+        return view('idea.index');
     }
 
     /**
---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,6 @@
+<?php return array (
+  'create-idea' => 'App\\Http\\Livewire\\CreateIdea',
+  'idea-index' => 'App\\Http\\Livewire\\IdeaIndex',
+  'idea-show' => 'App\\Http\\Livewire\\IdeaShow',
+  'ideas-index' => 'App\\Http\\Livewire\\IdeasIndex',
+);
