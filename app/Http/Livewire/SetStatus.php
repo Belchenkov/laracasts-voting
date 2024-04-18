efc19ae28f958c72bf93fb3a8f4b194dab5d2356
Алексей Бельченков <u608110@gmail.com>
2024-04-18 12:22:03 +0400

    Set Status Notify All Voters

--- app/Http/Livewire/SetStatus.php
+++ app/Http/Livewire/SetStatus.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use App\Mail\IdeaStatusUpdatedMailable;
 use App\Models\Idea;
 use Illuminate\Http\Response;
 use Livewire\Component;
+use Illuminate\Support\Facades\Mail;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class SetStatus extends Component
 {
     public $idea;
     public $status;
+    public $notifyAllVoters;
 
     public function mount(Idea $idea)
     {
@@ -27,7 +30,23 @@ class SetStatus extends Component
         $this->idea->status_id = $this->status;
         $this->idea->save();
 
+        if ($this->notifyAllVoters) {
+            $this->notifyAllVoters();
+        }
+
         $this->emit('statusWasUpdated');
+    }
+
+    public function notifyAllVoters()
+    {
+        $this->idea->votes()
+            ->select('name', 'email')
+            ->chunk(100, function ($voters) {
+                foreach ($voters as $user) {
+                    Mail::to($user)
+                        ->queue(new IdeaStatusUpdatedMailable($this->idea));
+                }
+            });
     }
 
     public function render(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
