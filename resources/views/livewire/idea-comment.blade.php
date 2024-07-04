c89dcc77dacb77724ab8ca0929c903da6949f896
Belchenkov <u608110@gmail.com>
2024-07-04 15:25:59 +0400

    Edit Comments

--- resources/views/livewire/idea-comment.blade.php
+++ resources/views/livewire/idea-comment.blade.php
@@ -21,29 +21,46 @@
                     @endif
                     <div>{{ $comment->created_at->diffForHumans() }}</div>
                 </div>
-                <div
-                    class="flex items-center space-x-2"
-                    x-data="{ isOpen: false }"
-                >
-                    <div class="relative">
-                        <button
-                            class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3"
-                            @click="isOpen = !isOpen"
-                        >
-                            <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
-                        </button>
-                        <ul
-                            class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl z-10 py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0"
-                            x-cloak
-                            x-show.transition.origin.top.left="isOpen"
-                            @click.away="isOpen = false"
-                            @keydown.escape.window="isOpen = false"
-                        >
-                            <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
-                            <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
-                        </ul>
+                @auth
+                    <div
+                        class="flex items-center space-x-2"
+                        x-data="{ isOpen: false }"
+                    >
+                        <div class="relative">
+                            <button
+                                class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3"
+                                @click="isOpen = !isOpen"
+                            >
+                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
+                            </button>
+                            <ul
+                                class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl z-10 py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0"
+                                x-cloak
+                                x-show.transition.origin.top.left="isOpen"
+                                @click.away="isOpen = false"
+                                @keydown.escape.window="isOpen = false"
+                            >
+                                @can('update', $comment)
+                                    <li>
+                                        <a
+                                            href="#"
+                                            @click.prevent="
+                                            isOpen = false
+                                            Livewire.emit('setEditComment', {{ $comment->id }})
+                                            {{-- $dispatch('custom-show-edit-modal') --}}
+                                        "
+                                            class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
+                                        >
+                                            Edit Comment
+                                        </a>
+                                    </li>
+                                @endcan
+                                <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
+                                <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
+                @endauth
             </div>
         </div>
     </div>
